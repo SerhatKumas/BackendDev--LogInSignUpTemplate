466674a43c812cc32bcff187f78c20fe06b7739a
Serhat Kumas <serhat.kumas@hotmail.com>
2022-01-06 17:18:53 +0300

    Entrance Handler and login page are developed

---
+++ Php-files/enteranceHandler.php
@@ -0,0 +1,49 @@
+<?php
+
+$servername = "localhost:3325";
+$username = "root";
+$password = "";
+$dbname = "loginproject";
+$userEmail = " " ;
+
+
+// Create connection
+$conn = new mysqli($servername, $username, $password,$dbname);
+
+// Check connection
+if ($conn->connect_error) {
+  die("Connection failed: " . $conn->connect_error);
+}
+
+//chech requset typw sent from html 
+if ($_SERVER["REQUEST_METHOD"]=="POST"){
+
+// take email from html form
+$userEmail = test_input($_POST["email"]) ;  
+
+//check whether email is existed in database
+$sql = "SELECT email FROM users WHERE email='".$userEmail."'";
+$result = $conn->query($sql);
+
+//if email existed direct to the Log in Page
+if ($result->num_rows > 0) {
+  header("Location: logIn.html");
+}
+//if not existed direct to the sign up page
+else{
+  header("Location: signUp.html");
+}
+
+}
+//close database conection
+$conn->close();
+
+//function that clans string from unwanted chars
+function test_input($data) {
+    $data = trim($data);
+    $data = stripslashes($data);
+    $data = htmlspecialchars($data);
+    return $data;
+  }
+
+?>
